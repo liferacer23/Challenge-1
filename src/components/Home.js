5e4f27ab6e42e63882ffa575eb4d1a82d992c4af
liferacer23 <binyamg23@gmail.com>
2021-11-20 17:03:52 +0300

    image-display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import {useState} from 'react';
 import image1t from '../images/image-product-1-thumbnail.jpg';
 import image1 from '../images/image-product-1.jpg';
 import image2t from '../images/image-product-2-thumbnail.jpg';
@@ -10,17 +11,27 @@ import image4t from '../images/image-product-4-thumbnail.jpg';
 import image4 from '../images/image-product-4.jpg';
 
 export default function Home() {
+
+    const [img_display,setImg_display]=useState([image1]);
+    console.log(img_display);
+
+
+    const change_image =(e)=>{
+            
+            setImg_display(e.target.src);
+            console.log(e.target.src);
+    }
     return (
         <><div className="body-container">
             <div className="image-section">
                 <div className="image-display">
-                    <img className="image-dispalyed" src={image1} alt="" />
+                    <img className="image-dispalyed" src={img_display} alt="" />
                 </div>
                 <div className="image-choices">
-                    <img className="image-choice" src={image1t} alt="" />
-                    <img className="image-choice" src={image2t} alt="" />
-                    <img className="image-choice" src={image3t} alt="" />
-                    <img className="image-choice" src={image4t} alt="" />
+                    <img onClick={change_image}className="image-choice" src={image1} alt="" />
+                    <img onClick={change_image} className="image-choice" src={image2} alt="" />
+                    <img onClick={change_image} className="image-choice" src={image3} alt="" />
+                    <img onClick={change_image} className="image-choice" src={image4} alt="" />
                 </div>
             </div>
             <div className="info-section">
@@ -39,7 +50,7 @@ export default function Home() {
                <div className='buttons'>
 
                        <button className="amount-btn">+</button>
-                       <button className="amount-btn" disabled>0</button>
+                       <button className="amount-btn amount" disabled>0</button>
                        <button className="amount-btn">-</button>
                         <button className="add-btn"> <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20"><path className="svg" d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#ffff" fill-rule="nonzero"/></svg> Add to cart</button>
 
